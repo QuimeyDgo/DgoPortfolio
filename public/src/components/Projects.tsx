6f9da991e125e0c1235b54368c828e50ce16a427
QuimeyDAgos <164278589+quimey2312@users.noreply.github.com>
2025-10-30 15:28:02 -0300

    Update Projects.tsx

--- public/src/components/Projects.tsx
+++ public/src/components/Projects.tsx
@@ -139,7 +139,7 @@ Al ser una aplicación de uso personal y sin conexión a servidores externos, Co
         {/* MODAL GLOBAL: fuera de la grilla para que cubra todo */}
         {expandedIndex !== null && (
           <div
-            className="fixed inset-0 z-50 bg-gray-950/80 backdrop-blur-sm flex justify-center items-center transition-all"
+            className="fixed inset-0 z-50 bg-gray-950/50 backdrop-blur-md backdrop-brightness-40 flex justify-center items-center transition-all"
             onClick={handleModalBackdrop}
             style={{animation: 'fadeinmodal .2s'}}
           >
